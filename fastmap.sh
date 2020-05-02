a32894c1acdf861fba6acad1e3e94c6962d91c18
f1s3c <59068073+f1s3c@users.noreply.github.com>
2020-05-02 17:45:57 +0100

    Update fastmap.sh
    Fixed conditional statement for the initial port scan

--- fastmap.sh
+++ fastmap.sh
@@ -3,7 +3,8 @@ fastmap ()
 	# $1 = IP
 	# $2 = BOX NAME
 	if [ "$#" -lt 2 ]; then
-		echo "Usage: map <IP> <BoxName> [-a](add to hosts) [-t,u,tu](tcp, udp, both)"
+		echo "Usage: map <IP> <BoxName>"
+		echo -e "\t[-a](add to hosts) [-t,u,tu](tcp, udp, both) [-n](just perform portscan)"
 		return 1
 	fi
 
@@ -13,24 +14,32 @@ fastmap ()
 		printf '%s	%s.htb %s' "$1" "$2" "$2" >> /etc/hosts
 	fi
 
-	echo -e "\n[*] Performing initial port scan"
-	ports=$(nmap -p- --min-rate=1000 -T4 "$1" | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
-
-	echo "[!] Open ports: $ports."
-	
 	if [ ! -d "nmap" ]; then
 		mkdir nmap
 	fi
-	
+
+	echo -e "\n[*] Performing initial port scan"
 	if [[ "$*" == *"-u"* ]]; then
-		echo -e "\n[*] Doing UDP scan...\n"
-		nmap -sC -sU -sV -p $ports -oA nmap/"$2" "$1"
-	elif [[ "$*" = *"-tu"* ]]; then
-		echo -e "\n[*] Doing TCP and UDP scan...\n"
-		nmap -sC -sU -sT -sV -p $ports -oA nmap/"$2" "$1"
+		ports=$(nmap -p- -sU --min-rate=1000 -T4 "$1" | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
+		switch="-sU"
+		protocol="UDP"
+	elif [[ "$*" == *"-tu"* ]]; then
+		ports=$(nmap -p- -sU -sT --min-rate=1000 -T4 "$1" | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
+		switch="-sU -sT"
+		protoco="TCP and UDP"
 	else
-		echo -e "\n[*] Doing TCP scan...\n"
-		nmap -sC -sV -p $ports -oA nmap/"$2" "$1"
+		ports=$(nmap -p- --min-rate=1000 -T4 "$1" | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
+		switch="-sT"
+		protocol="TCP"
+	fi
+
+	echo "Open ports on $protocol: $ports."
+
+	if [[ "$*" == *"-n"* ]]; then
+		return 2
+	else
+		echo -e "\n[*] Performing detailed $protocol scan...\n"
+		nmap $switch -sC -sV -p $ports -oA nmap/"$2" "$1" 
 	fi
 
 	echo -e "\n[+] All done!"
